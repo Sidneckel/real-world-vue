fa502def3595372cadf50164000885111d2b5f1a
sidneckel <sidn.ick2015@gmail.com>
2024-08-02 10:47:17 -0300

    render

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="layout">
+    <header>
+      <div class="wrapper">
+  <nav>
+    <RouterLink :to="{ name: 'home' }">Events</RouterLink> |
+    <RouterLink :to="{ name: 'about' }">About</RouterLink>
+  </nav>
+</div>
+    </header>
+    <RouterView />
+  </div>
+</template>
+
+<style>
+
+#layout {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+h2 {
+  font-size: 20px;
+}
+nav {
+  padding: 30px;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
